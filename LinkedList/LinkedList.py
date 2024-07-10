a503bf82beeac3a711c364e97c7d85f52f1ed496
mileoa <ezkimozzzzz@gmail.com>
2024-07-10 23:36:24 +1000

    Добавил ответ на задание

---
+++ LinkedList/LinkedList.py
@@ -0,0 +1,95 @@
+class Node:
+
+    def __init__(self, v):
+        self.value = v
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_in_tail(self, item):
+        if self.head is None:
+            self.head = item
+        else:
+            self.tail.next = item
+        self.tail = item
+
+    def print_all_nodes(self):
+        node = self.head
+        while node != None:
+            print(node.value)
+            node = node.next
+
+    def find(self, val):
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                return node
+            node = node.next
+        return None
+
+    def find_all(self, val):
+        result = []
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                result.append(node)
+            node = node.next
+        return result
+
+    def delete(self, val, all=False):
+        node = self.head
+        while node is not None:
+            if node.value == val and node is self.head:
+                if self.head is self.tail:
+                    self.tail = None
+                    self.head = None
+                    break
+                self.head = node.next
+                if not all:
+                    break
+            elif node.next is None:
+                break
+            elif node.next.value == val:
+                if node.next is self.tail:
+                    node.next = None
+                    self.tail = node
+                    break
+                next_node = node.next.next
+                node.next = next_node
+                if not all:
+                    break
+
+            node = node.next
+
+    def clean(self):
+        node = self.head
+        while self.head is not None:
+            self.head = node.next
+            node = self.head
+        self.tail = None
+
+    def len(self):
+        result = 0
+        node = self.head
+        while node is not None:
+            result += 1
+            node = node.next
+        return result
+
+    def insert(self, afterNode, newNode):
+        if afterNode is None:
+            if self.head is None:
+                self.tail = newNode
+            newNode.next = self.head
+            self.head = newNode
+        else:
+            next_node = afterNode.next
+            afterNode.next = newNode
+            newNode.next = next_node
+            if afterNode is self.tail:
+                self.tail = newNode
---
+++ LinkedList/LinkedListTests.py
@@ -0,0 +1,108 @@
+import unittest
+import random
+
+from LinkedList import LinkedList, Node
+
+
+class LinkedListTests(unittest.TestCase):
+
+    def setUp(self):
+        self.empty_linked_list = LinkedList()
+
+        self.one_el_linked_list = LinkedList()
+        self.one_el_linked_list.add_in_tail(Node(10))
+
+        self.lot_el_linked_list = LinkedList()
+        for i in range(1000):
+            self.lot_el_linked_list.add_in_tail(Node(i))
+
+    def test_regression_delete_empty_list(self):
+        self.empty_linked_list.delete(1, False)
+        self.assertEqual(self.empty_linked_list.head, None)
+        self.assertEqual(self.empty_linked_list.tail, None)
+
+        self.empty_linked_list.delete(1, True)
+        self.assertEqual(self.empty_linked_list.head, None)
+        self.assertEqual(self.empty_linked_list.tail, None)
+
+    def test_regression_delete_one_el_list(self):
+        self.one_el_linked_list.delete(11, False)
+        self.assertEqual(self.one_el_linked_list.head.value, 10)
+        self.assertEqual(self.one_el_linked_list.tail.value, 10)
+
+        self.one_el_linked_list.delete(10, False)
+        self.assertEqual(self.one_el_linked_list.head, None)
+        self.assertEqual(self.one_el_linked_list.tail, None)
+
+    def test_regression_delete_lot_el_list(self):
+        self.lot_el_linked_list.delete(11, False)
+        self.assertEqual(self.lot_el_linked_list.head.value, 0)
+        self.assertEqual(self.lot_el_linked_list.tail.value, 999)
+        for i in range(1000):
+            if i == 11:
+                self.assertEqual(self.lot_el_linked_list.find(11), None)
+                continue
+            self.assertEqual(self.lot_el_linked_list.find(i).value, i)
+
+        self.lot_el_linked_list.add_in_tail(Node(0))
+        self.lot_el_linked_list.delete(0, True)
+        self.assertEqual(self.lot_el_linked_list.head.value, 1)
+        self.assertEqual(self.lot_el_linked_list.tail.value, 999)
+        for i in range(1000):
+            if i == 0 or i == 11:
+                self.assertEqual(self.lot_el_linked_list.find(0), None)
+                continue
+            self.assertEqual(self.lot_el_linked_list.find(i).value, i)
+
+        self.lot_el_linked_list.add_in_tail(Node(1))
+        self.lot_el_linked_list.delete(1, False)
+        self.assertEqual(self.lot_el_linked_list.head.value, 2)
+        self.assertEqual(self.lot_el_linked_list.tail.value, 1)
+
+    def test_regression_empty_linked_list(self):
+        self.empty_linked_list.clean()
+        self.assertEqual(self.empty_linked_list.head, None)
+        self.assertEqual(self.empty_linked_list.tail, None)
+
+    def test_regression_clean_one_el_list(self):
+        self.one_el_linked_list.clean()
+        self.assertEqual(self.one_el_linked_list.head, None)
+        self.assertEqual(self.one_el_linked_list.tail, None)
+
+    def test_regression_clean_lot_el_list(self):
+        self.lot_el_linked_list.clean()
+        self.assertEqual(self.lot_el_linked_list.head, None)
+        self.assertEqual(self.lot_el_linked_list.tail, None)
+
+    def test_regression_find_all_empty_linked_list(self):
+        arr = self.empty_linked_list.find_all(0)
+        self.assertEqual(len(arr), 0)
+
+    def test_regression_find_all_one_el_list(self):
+        arr = self.one_el_linked_list.find_all(0)
+        self.assertEqual(len(arr), 0)
+        arr = self.one_el_linked_list.find_all(10)
+        self.assertEqual(len(arr), 1)
+        for i in arr:
+            self.assertEqual(i.value, 10)
+
+    def test_regression_find_all_lot_el_list(self):
+        self.lot_el_linked_list.add_in_tail(Node(0))
+        arr = self.lot_el_linked_list.find_all(0)
+        self.assertEqual(len(arr), 2)
+        for i in arr:
+            self.assertEqual(i.value, 0)
+
+    def test_regression_len(self):
+        self.assertEqual(self.empty_linked_list.len(), 0)
+        self.assertEqual(self.one_el_linked_list.len(), 1)
+        self.assertEqual(self.lot_el_linked_list.len(), 1000)
+
+    def tearDown(self):
+        self.empty_linked_list = None
+        self.one_el_linked_list = None
+        self.lot_el_linked_list = None
+
+
+if __name__ == "__main__":
+    unittest.main()
