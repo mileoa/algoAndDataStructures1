24d2fe8efa743fab56049cf7640e3caf63a77d9f
mileoa <ezkimozzzzz@gmail.com>
2024-07-22 17:45:10 +1000

    Добавил ответ на задание

---
+++ PowerSet/PowerSet.py
@@ -0,0 +1,58 @@
+class PowerSet:
+
+    def __init__(self):
+        self.slots = {}
+        # ваша реализация хранилища
+
+    def size(self):
+        return len(self.slots)
+
+    def put(self, value):
+        self.slots[value] = None
+
+    def get(self, value):
+        return value in self.slots
+
+    def remove(self, value):
+        if not self.get(value):
+            return False
+        del self.slots[value]
+        return True
+
+    def intersection(self, set2):
+        result = PowerSet()
+        for el in set2.slots:
+            if el in self.slots:
+                result.put(el)
+        return result
+
+    def union(self, set2):
+        result = PowerSet()
+
+        for i in self.slots:
+            if i not in result.slots:
+                result.put(i)
+
+        for i in set2.slots:
+            if i not in result.slots:
+                result.put(i)
+
+        return result
+
+    def difference(self, set2):
+        result = PowerSet()
+
+        for i in self.slots:
+            if i not in set2.slots:
+                result.put(i)
+
+        for i in set2.slots:
+            if i not in self.slots:
+                result.put(i)
+        return result
+
+    def issubset(self, set2):
+        for i in set2.slots:
+            if i not in self.slots:
+                return False
+        return True
---
+++ PowerSet/PowerSetTests.py
@@ -0,0 +1,151 @@
+import unittest
+import random
+
+from PowerSet import PowerSet
+
+
+class PowerSetTests(unittest.TestCase):
+
+    def test_regression_put(self):
+        ps = PowerSet()
+        for i in range(2001):
+            ps.put(i)
+            self.assertEqual(ps.size(), i + 1)
+            self.assertTrue(i in ps.slots)
+        self.assertEqual(ps.slots, dict(((i, None) for i in range(2001))))
+
+        for i in range(2001):
+            ps.put(i)
+        self.assertEqual(ps.size(), 2001)
+        self.assertEqual(ps.slots, dict(((i, None) for i in range(2001))))
+
+    def test_regression_remove(self):
+        ps = PowerSet()
+        for i in range(2001):
+            ps.put(i)
+
+        self.assertTrue(ps.remove(6))
+        self.assertNotIn(6, ps.slots)
+        self.assertEqual(ps.size(), 2000)
+
+        self.assertFalse(ps.remove(6))
+        self.assertEqual(ps.size(), 2000)
+
+    def test_regression_intersection(self):
+        ps1 = PowerSet()
+        ps2 = PowerSet()
+
+        for i in range(0, 2001, 2):
+            ps1.put(i)
+        for i in range(2001):
+            ps2.put(i)
+
+        self.assertTrue(isinstance(ps1.intersection(ps2), PowerSet))
+        self.assertEqual(
+            ps1.intersection(ps2).slots, dict(((i, None) for i in range(0, 2001, 2)))
+        )
+
+    def test_empty_intersection(self):
+        ps1 = PowerSet()
+        ps2 = PowerSet()
+
+        self.assertEqual(ps1.intersection(ps2).slots, {})
+
+        for i in range(2001):
+            ps2.put(i)
+
+        self.assertEqual(ps1.intersection(ps2).slots, {})
+
+    def test_empty_union(self):
+        ps1 = PowerSet()
+        ps2 = PowerSet()
+
+        self.assertEqual(ps1.union(ps2).slots, {})
+
+        for i in range(2001):
+            ps2.put(i)
+
+        self.assertEqual(
+            ps1.union(ps2).slots,
+            dict(((i, None) for i in range(2001))),
+        )
+        self.assertEqual(
+            ps2.union(ps1).slots,
+            dict(((i, None) for i in range(2001))),
+        )
+
+    def test_regression_union(self):
+        ps1 = PowerSet()
+        ps2 = PowerSet()
+
+        for i in range(0, 2001, 2):
+            ps1.put(i)
+        for i in range(1, 2001, 2):
+            ps2.put(i)
+
+        self.assertTrue(isinstance(ps1.union(ps2), PowerSet))
+        self.assertEqual(ps1.union(ps2).slots, dict(((i, None) for i in range(2001))))
+
+    def test_empty_difference(self):
+        ps1 = PowerSet()
+        ps2 = PowerSet()
+
+        self.assertEqual(ps1.difference(ps2).slots, {})
+
+        for i in range(2001):
+            ps1.put(i)
+
+        self.assertTrue(isinstance(ps1.difference(ps2), PowerSet))
+        self.assertEqual(
+            ps1.difference(ps2).slots, dict(((i, None) for i in range(2001)))
+        )
+        self.assertEqual(
+            ps2.difference(ps1).slots, dict(((i, None) for i in range(2001)))
+        )
+
+    def test_regression_difference(self):
+        ps1 = PowerSet()
+        ps2 = PowerSet()
+
+        for i in range(0, 2001, 2):
+            ps1.put(i)
+        for i in range(1, 2001, 2):
+            ps2.put(i)
+
+        ps1.put(2002)
+        ps2.put(2002)
+
+        self.assertTrue(isinstance(ps1.difference(ps2), PowerSet))
+        self.assertEqual(
+            ps1.difference(ps2).slots, dict(((i, None) for i in range(2001)))
+        )
+
+    def test_empty_issubset(self):
+        ps1 = PowerSet()
+        ps2 = PowerSet()
+        self.assertTrue(ps2.issubset(ps1))
+
+        for i in range(2000):
+            ps1.put(i)
+
+        self.assertFalse(ps2.issubset(ps1))
+        self.assertTrue(ps1.issubset(ps2))
+
+    def test_regression_issubset(self):
+        ps1 = PowerSet()
+        ps2 = PowerSet()
+
+        for i in range(2000):
+            ps1.put(i)
+        for i in range(2001):
+            ps2.put(i)
+
+        self.assertTrue(ps2.issubset(ps1))
+        self.assertFalse(ps1.issubset(ps2))
+
+        ps1.put(2000)
+        self.assertTrue(ps1.issubset(ps2))
+
+
+if __name__ == "__main__":
+    unittest.main()
